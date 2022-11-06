0b369cf96921bdd6d308e00fa6ef7f02779fa9ee
Jeff Lin <jeff.online.lin@gmail.com>
2022-11-05 18:05:08 -0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by ying lin on 11/5/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
